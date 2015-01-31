b1aca1ba518e43e74da35205642fa55ec5fd21d3
lutherism <xanderjanz@gmail.com>
2015-01-30 22:49:36 -0800

    queryEngine design

--- docs/schema.md
+++ docs/schema.md
@@ -12,8 +12,8 @@ Surface that database via a Relay-able API.
 }
 ```
 
-Nodes hold most of the data that we use in our app. Any piece of data could be represented as a node, from a User to a Video to a Server event.
-
+Nodes hold most of the data that we use in our app. Any piece of data could be
+represented as a node, from a User to a Video to a Server event.
 
 
 ### Connection JSON
@@ -26,24 +26,29 @@ Nodes hold most of the data that we use in our app. Any piece of data could be r
     }
 }
 ```
-#####NodeID
-A uniq
 
 
-Connections are small objects that point at a node. They can carry any amount of data, but require an id field. The CursorID is used to identify a connection within a list.
+Connections are small objects that point at a node. They can carry any amount of
+ data, but only require an id:NodeID field. The CursorID is used to identify a
+ connection within a list.
 
 ### Query JSON
 ```sh
 {
     NodeID: {
         attributes: true,
-        edge: {
-            cursor: true,
-            node: {
-                attributes: true
-            }
+        edge: [{ //query options
+                options: true
+            }, { //object model
+                cursor: true,
+                node: {
+                    attributes: true
+                }
+            }]
         }
     }
 }
 ```
 
+#####NodeID
+A uniq node identifier
--- source/js/modules/queryEngine.js
+++ source/js/modules/queryEngine.js
@@ -1,13 +1,104 @@
 var queryEngine,
-  mongoose = require('mongoose');
+  db = require('db');
 
-//take in schema query obejcts, spit out mongo queries
-
-queryEngine = function queryEngine(query, opts) {
-  mongoQuery = query;
-
-
-  return mongoQuery;
+function isEdge(value) { //duck type edge objects
+  return !!(
+    value &&
+    Array.isArray(value) &&
+    value.length === 2 &&
+    typeof value[0] === 'object' &&
+    typeof value[1] === 'object'
+  );
 }
 
-modules.epxorts = queryEngine;
+function map(obj, fn) {
+  var newObj = {};
+  for(var i in obj) {
+    newObj[i] = fn(obj[i], i, obj);
+  }
+  return newObj;
+}
+
+function filter(obj, filter) {
+  var ret = {};
+  map(filter, function(a, v, k) {
+    if (v === true) a[k] = obj[k];
+  });
+  return ret;
+}
+
+function reduce(obj, fn, a) {
+  map(obj, function(v, k) {
+    a = fn(a, v, k);
+  });
+}
+
+function hasAll(obj, compare) {
+  var ret = true;
+  for (var i in compare) {
+    ret = ret && Object.prototype.hasOwnProperty.call(obj, i);
+  }
+  return ret;
+}
+
+
+/*
+* Fetches the given NodeID, and recursively fetches any subsequent edges in node
+* Once done building tree, calls callback with tree as arg
+*/
+function queryNode(id, node, callback) {
+  var edges = {},
+    attributes = {};
+  map(node, function(v, k) {
+    if (isEdge(v)) {
+      edges[k] = v
+    } else {
+      attributes[k] = v;
+    }
+  });
+
+  db.get(id, function(model) {
+    var ret = filter(model, attributes);
+
+    map(edges, function(v, k) {
+
+      queryEdge(model[k], v, function(connections) {
+        ret[k] = connections;
+
+        if (hasAll(ret, edges)) {
+          callback(ret);
+        }
+      });
+    }
+  });
+}
+
+function queryEdge(edge, query, callback) {
+  var options = query[0],
+    node = query[1],
+    connections = [];
+
+  edge.map(function(connection, i) {
+    queryNode(connection.node.id, node, function(model) {
+      connections[i] = model;
+      if (hasAll(connections, edge)) {
+        callback(connections);
+      }
+    });
+  });
+};
+//take in schema query obejcts, spit out mongo queries
+
+queryEngine = function queryEngine(queries, callback) {
+  var results = {};
+  queries.map(function(node, id) {
+    queryNode(id, node, function(result) {
+      results[id] = result;
+      if (hasAll(queries, results)) {
+        callback(results);
+      }
+    })
+  });
+}
+
+modules.exports = queryEngine;
